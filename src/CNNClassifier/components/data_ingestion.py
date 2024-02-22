511af5cd5df414255bd7f722ed10c23c959dbe0c
pramodkhavare <pramodkhavare2000@gmail.com>
2024-02-22 11:06:37 +0530

    Data_ingestion Commit

--- src/CNNClassifier/components/data_ingestion.py
+++ src/CNNClassifier/components/data_ingestion.py
@@ -1,1 +1,22 @@
+import os ,sys 
+from CNNClassifier.utils.utils import *
+from CNNClassifier.logger import logging 
+from CNNClassifier.exception import CustomException 
+from tqdm import tqdm
+from CNNClassifier.config import *
 
+class DataIngestion():
+    def __init__(self):
+        pass 
+
+    def download_file(self):
+        pass 
+
+    def get_updated_list_of_file(self):
+        pass 
+    def preprocess(self):
+        pass 
+    def unzip_and_clean(self):
+        pass 
+
+# D:\Deep Learning\CatvsDogClassifier\src\CNNClassifier\components\data_ingestion.py
