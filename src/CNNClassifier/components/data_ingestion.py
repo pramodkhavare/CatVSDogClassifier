35f8ecfe6dc577b3bce5ca1c3146de2e03119104
pramodkhavare <pramodkhavare2000@gmail.com>
2024-02-22 20:16:16 +0530

    Data_ingestion Commit

--- src/CNNClassifier/components/data_ingestion.py
+++ src/CNNClassifier/components/data_ingestion.py
@@ -1,23 +1,50 @@
-import os ,sys 
-from CNNClassifier.utils.utils import read_yaml ,create_directory
-from CNNClassifier.logger import logging 
-from CNNClassifier.exception import CustomException 
-from tqdm import tqdm
-from CNNClassifier.config import *
 
-class DataIngestion():
-    def __init__(self):
-        pass 
+from CNNClassifier.constant import PARAM_FILE_PATH ,CONFIG_FILE_PATH
+import os 
+import urllib.request as request 
+from zipfile import ZipFile 
+from CNNClassifier.logger import logging 
+from pathlib import Path 
+import tqdm as tqdm
+from CNNClassifier.config.configuration import ConfigurationManager
+
+class DataIngestion :
+    def __init__(self ,config:ConfigurationManager):
+        self.config = config 
 
     def download_file(self):
-        pass 
+        if not os.path.exists(self.config.local_data_file):
+            filenmae , url = request.urlretrieve(
+                url=self.config.source_URL ,
+                filename= self.config.local_data_file
+            )
 
-    def get_updated_list_of_file(self):
-        pass 
-    def preprocess(self):
-        pass 
+
+    def get_updated_list_of_file(self ,list_of_files):
+        updated_list_of_file = [f for f in list_of_files if f.endswith('.jpg') and ("cat" or "dog" in f) ]
+        return updated_list_of_file 
+    
+
+    
+    def preprocess(self ,zf:ZipFile ,f:str ,working_dir :str):
+        target_filepath =os.path.join(working_dir ,f)
+        if not os.path.exists(target_filepath):
+            zf.extract(f ,working_dir)
+
+        if os.path.getsize == 0:
+            os.remove(target_filepath)        
+
+
+
     def unzip_and_clean(self):
-        pass 
+        with ZipFile(file=self.config.local_data_file , mode='r') as zf:
+            list_of_file = zf.namelist()
+            updated_list_of_file = self.get_updated_list_of_file(list_of_files=list_of_file)
+
+            for f in updated_list_of_file :
+                self.preprocess(f=f ,zf=zf ,working_dir=self.config.unzip_dir)
+
+
 
 # D:\Deep Learning\CatvsDogClassifier\src\CNNClassifier\components\data_ingestion.py
     
