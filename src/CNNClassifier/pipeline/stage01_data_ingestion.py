a95e02a04c9e87f8dcae4846a17d3c992c721fcf
pramodkhavare <pramodkhavare2000@gmail.com>
2024-02-22 20:28:17 +0530

    Pipeline1 Commit

---
+++ src/CNNClassifier/pipeline/stage01_data_ingestion.py
@@ -0,0 +1,24 @@
+from CNNClassifier.components.data_ingestion import DataIngestion 
+from CNNClassifier.config.configuration import ConfigurationManager 
+from CNNClassifier.logger import logging
+from CNNClassifier.exception import CustomException 
+import os ,sys
+process = 'DataIngestion'
+
+
+
+
+
+def main():
+    try:
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.unzip_and_clean()
+    except Exception as e:
+        logging.info("Something wrong in {process} process")
+        raise CustomException(e ,sys)
+
+if __name__ == '__main__':
+    main()
